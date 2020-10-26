7dab8c13ce44ffdcd96567c4007d914c00c3a7dc
TSerup <au575786@post.au.dk>
2020-10-26 16:01:44 +0100

    Second commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
+import React, {useState, useEffect} from 'react';
+import { Button, Text, View, StyleSheet} from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import styles from "./styles"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App () {
+
+const [isInternetReachable, setIsInternetReachable] = useState(false);
+const [strength, setStrength] = useState(0);
+const [type, setType] = useState("");
+const [ip, setIp] = useState("");
+const [icon, setIcon] = useState("wifi-strength-off");
+
+
+useEffect(() => {
+  NetInfo.addEventListener(state => {
+    setStrength(state.details.strength);
+    setIsInternetReachable(state.isInternetReachable);
+    setType(state.type);
+    setIp(state.details.ipAddress)
+    setIcon(getNetworkIcon());
+  });
+});
+
+const getNetworkIcon=()=> {
+  if(!strength)
+    return "wifi-strength-off"
+  else if (Number(strength) <= 40) 
+    return "wifi-strength-1"
+  else if (Number(strength) <= 60) 
+    return "wifi-strength-2"
+  else if (Number(strength) <= 80) 
+    return "wifi-strength-3"
+  else if (Number(strength) <= 100) 
+    return "wifi-strength-4"
+  else if(!isInternetReachable)
+    return "network-strength-outline"
+  else
+    return "error";
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+return (
+  <View style={styles.container}>
+    <Text style={styles.heading1}>Type of network: {type}</Text>
+    <MaterialCommunityIcons name={icon} size={125}/> 
+    <Text style={styles.heading2}>Network strength: {strength}</Text>
+    <Text style={styles.heading2}>Device ip: {ip}</Text>  
+  </View>
+  );
+}
---
+++ styles.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading1:{
+    color:"#000",
+    fontWeight:"bold",
+    fontSize:30,
+    margin:20
+  },
+  heading2:{
+    color:"#000",
+    margin:5,
+    fontWeight:"bold",
+    fontSize:15
+  },
+  });
+  export default styles;
